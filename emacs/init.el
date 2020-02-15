2d61e729729d8afb565c7c39c40c96be7f516453
Masaya Tojo <masaya@tojo.tokyo>
2020-02-15 13:50:18 +0900

    Emacs で Twitter をする

--- emacs/init.el
+++ emacs/init.el
@@ -306,4 +306,7 @@ There are two things you can do about this warning:
     (interactive "nTransparency Value 0 - 100 opaque:")
     (set-frame-parameter (selected-frame) 'alpha value))
 
-  (transparency 80))
+  (transparency 80)
+
+  (use-package twittering-mode
+    :ensure t))
