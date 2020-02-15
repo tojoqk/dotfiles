494851e7cd8bd7b7e205135d66560f50f35c2efd
Masaya Tojo <masaya@tojo.tokyo>
2020-02-15 13:51:03 +0900

    screen の良い感じの設定を追加する

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ ln -sf ~/git/dotfiles/uim ~/.uim
 ln -sf ~/git/dotfiles/tmux.conf ~/.tmux.conf
 mkdir -p ~/.config/dunst
 ln -sf ~/git/dotfiles/dunstrc ~/.config/dunst/dunstrc
+ln -sf ~/git/dotfiles/screenrc ~/.screenrc
